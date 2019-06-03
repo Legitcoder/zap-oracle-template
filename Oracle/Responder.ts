02fd58f973d9def61461d8c1802803f96b2437b9
Moin Uddin <U_Moin75@yahoo.com>
2019-06-03 17:30:31 -0400

    Write Basic Oracle fetch data from Pokemon API

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,6 +1,20 @@
+const axios = require("axios");
 
-export async function getResponse(query:string,params?:string[]|[]){
-	//Get data based on the query string and Parameters
-		return ['response here']
+export async function getResponse(query:string,params:string[]|[]){
+	var name = params[0];
+	var response;
+	switch (query) {
+		case "getWeight":
+			response = await axios.get('https://pokeapi.co/api/v2/pokemon/' + name);
+			console.log(response);
+			break;
+		default:
+			console.log("Error with Query");
+			break;
+	}
 
+	console.log([""+response.data.weight]);
+	return ["" + response.data.weight];
 }
+
+var x = getResponse("getWeight", ["charizard"]);
